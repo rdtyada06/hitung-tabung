96ccfd17a7bf539aac1f2a493eb58219e8b0217e
Rdtyada <raditya9arkananta@gmail.com>
2024-11-26 16:26:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Menghitung :blue[Volume Tabung] :sunglasses:")
+
+r = st.number_input("Masukan Jari-Jari (cm): ",0)
+t = st.number_input("Masukan Tinggi (cm): ",0)
